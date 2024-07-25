7b9af43cd369d411180a5bba67cbb7e9d9476054
MamykinVsevolod <vsevolodmamykin@gmail.com>
2024-07-25 22:13:31 +0300

    I made the location of the icon inside the rectangle

--- app/src/main/java/com/example/cellular_filling/ui/components/RectangleItemView.kt
+++ app/src/main/java/com/example/cellular_filling/ui/components/RectangleItemView.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -19,7 +21,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.example.cellular_filling.R
 import com.example.cellular_filling.model.RectangleItem
@@ -66,39 +67,41 @@ fun getPictureForType(type: RectangleType): Painter {
 
 @Composable
 fun RectangleItemView(rectangle: RectangleItem) {
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
+            .height(72.dp)
             .padding(start = 16.dp, end = 16.dp)
-            .background(color = Color.Black)
             .clip(
-                RoundedCornerShape(
-                    topStart = 8.dp,
-                    topEnd = 8.dp,
-                    bottomStart = 8.dp,
-                    bottomEnd = 8.dp
-                )
+                RoundedCornerShape(8.dp)
             )
+            .background(color = Color.Black),
+            contentAlignment = Alignment.CenterStart
     ) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .padding(start = 16.dp)
-                .clip(shape = CircleShape)
-                .background(getColorForType(type = rectangle.type))
-        ) {
-            Image(
-                painter = getPictureForType(type = rectangle.type),
-                contentDescription = "Picture",
-                modifier = Modifier.size(40.dp)
-            )
-        }
-        Column(modifier = Modifier
-            .padding(start = 16.dp)){
-            Text(text = "", style = Typography.bodyMedium)
-            Text(text = "", style = Typography.bodySmall)
+        Row {
+            Box(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .size(40.dp)
+                    .clip(shape = CircleShape)
+                    .background(getColorForType(type = rectangle.type))
+            ) {
+                Image(
+                    painter = getPictureForType(type = rectangle.type),
+                    contentDescription = "Picture",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+            ) {
+                Text(text = "", style = Typography.bodyMedium)
+                Text(text = "", style = Typography.bodySmall)
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
@@ -107,7 +110,7 @@ fun PreviewRectangleItemView() {
     RectangleItemView(
         rectangle = RectangleItem(
             id = 1,
-            type = RectangleType.ALIVE
+            type = RectangleType.LIFE
         )
     )
 }
