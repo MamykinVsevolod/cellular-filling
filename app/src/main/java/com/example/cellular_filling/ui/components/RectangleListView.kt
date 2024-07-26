5311d2f07cfe1adeed652e89224f4024f82ebd2b
MamykinVsevolod <vsevolodmamykin@gmail.com>
2024-07-26 21:08:34 +0300

    Working version with animation, but not for death

--- app/src/main/java/com/example/cellular_filling/ui/components/RectangleListView.kt
+++ app/src/main/java/com/example/cellular_filling/ui/components/RectangleListView.kt
@@ -1,15 +1,45 @@
 package com.example.cellular_filling.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.cellular_filling.model.RectangleItem
+import com.example.cellular_filling.model.RectangleType
+import kotlinx.coroutines.delay
 
 @Composable
 fun RectangleListView(items: List<RectangleItem>) {
+    val visibleItems = remember { mutableSetOf<RectangleItem>() }
     LazyColumn() {
         items(items.size) { index ->
             val item = items[index]
-            RectangleItemView(rectangle = item)
+            var isVisible by remember { mutableStateOf(item in visibleItems) }
+
+            AnimatedVisibility(
+                visible = isVisible,
+                enter = fadeIn() + expandHorizontally()
+            ) {
+                RectangleItemView(rectangle = item)
+            }
+            LaunchedEffect(item) {
+                if (item !in visibleItems && item.type != RectangleType.DEATH) {
+                    if (item.type == RectangleType.LIFE) {
+                        delay(500)
+                    }
+                    isVisible = true
+                    visibleItems.add(item)
+                } else {
+                    isVisible = true
+                }
+            }
         }
     }
 }
